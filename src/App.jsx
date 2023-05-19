057b9b825ddafa6e99189fbd9ae411111e0b3903
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2023-05-20 01:46:16 +0530

    Final build?

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Favicon from "react-favicon";
 
 function App() {
   return (
     <div className="min-h-screen p-10 text-white bg-gray-900 ">
+      <Favicon url="https://em-content.zobj.net/thumbs/160/apple/33/personal-computer_1f4bb.png" />
       <div className="fixed sm:block hidden top-0 left-0 w-full h-[10vh] z-30 bg-gray-900"></div>
       <Nav />
       <Hero />
